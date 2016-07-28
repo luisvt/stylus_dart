cb341bac6bb113f4290921ed0b5407e280cda95c
Luis Vargas <luisvargastijerino@gmail.com>
2016-07-28 04:09:47 -0400

    third commit

--- lib/nodes/if.dart
+++ lib/nodes/if.dart
@@ -19,64 +19,68 @@ import './node.dart' show Node;
  * @api public
  */
 
-class If {
-	If(cond, negate) {
-  Node.call(this);
-  this.cond = cond;
-  this.elses = [];
-  if (negate && negate.nodeName) {
-    this.block = negate;
-  } else {
-    this.negate = negate;
+class If extends Node {
+  var cond;
+
+  List elses;
+
+  var block;
+
+  var negate;
+
+  var postfix;
+
+  If([cond, negate]) {
+//  Node.call(this);
+    this.cond = cond;
+    this.elses = [];
+    if (negate && negate.nodeName) {
+      this.block = negate;
+    } else {
+      this.negate = negate;
+    }
   }
-	}
-}
 
-/**
- * Inherit from `Node.prototype`.
- */
+  /**
+   * Return a clone of this node.
+   *
+   * @return {Node}
+   * @api public
+   */
 
-If.prototype.__proto__ = Node.prototype;
+  clone(parent) {
+    var clone = new If();
+    clone.cond = this.cond.clone(parent, clone);
+    clone.block = this.block.clone(parent, clone);
+    clone.elses = this.elses.map((node) {
+      return node.clone(parent, clone);
+    });
+    clone.negate = this.negate;
+    clone.postfix = this.postfix;
+    clone.lineno = this.lineno;
+    clone.column = this.column;
+    clone.filename = this.filename;
+    return clone;
+  }
 
-/**
- * Return a clone of this node.
- * 
- * @return {Node}
- * @api public
- */
+  /**
+   * Return a JSON representation of this node.
+   *
+   * @return {Object}
+   * @api public
+   */
 
-clone(parent) {
-
-  var clone = new If();
-  clone.cond = this.cond.clone(parent, clone);
-  clone.block = this.block.clone(parent, clone);
-  clone.elses = this.elses.map((node){ return node.clone(parent, clone); });
-  clone.negate = this.negate;
-  clone.postfix = this.postfix;
-  clone.lineno = this.lineno;
-  clone.column = this.column;
-  clone.filename = this.filename;
-  return clone;
-}
-
-/**
- * Return a JSON representation of this node.
- *
- * @return {Object}
- * @api public
- */
-
-toJSON() {
-
-  return {
-    '__type': 'If',
-    'cond': this.cond,
-    'block': this.block,
-    'elses': this.elses,
-    'negate': this.negate,
-    'postfix': this.postfix,
-    'lineno': this.lineno,
-    'column': this.column,
-    'filename': this.filename
-  };
+  toJSON() {
+    return {
+      '__type': 'If',
+      'cond': this.cond,
+      'block': this.block,
+      'elses': this.elses,
+      'negate': this.negate,
+      'postfix': this.postfix,
+      'lineno': this.lineno,
+      'column': this.column,
+      'filename': this.filename
+    };
+  }
 }
